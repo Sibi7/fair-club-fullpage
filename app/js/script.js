962313012e98ab8bee1e8e34bb2e0350cd685c7e
Vladimir__Lipodat <sibi_7@mail.ru>
2017-08-03 17:42:36 +0300

    add slide

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        menu: '#menu',
+        lockAnchors: true,
+        anchors:['firstPage', 'secondPage'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['1', '2', '3', '4', '5'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+        verticalCentered: true
+    });
 });
