545bde914f1c6592cdc1d5afca3b26dccc74ee74
Vladimir__Lipodat <sibi_7@mail.ru>
2017-08-03 22:04:22 +0300

    add slide 2

--- app/js/script.js
+++ app/js/script.js
@@ -11,4 +11,39 @@ $(document).ready(function() {
         slidesNavPosition: 'bottom',
         verticalCentered: true
     });
+    $('.tabs ul li').click(function () {
+        $('.tabs ul li').addClass('active')
+
+    })
+});
+(function($){
+    jQuery.fn.lightTabs = function(options){
+
+        var createTabs = function(){
+            tabs = this;
+            i = 0;
+
+            showPage = function(i){
+                $(tabs).children("div").children("div").hide();
+                $(tabs).children("div").children("div").eq(i).show();
+                $(tabs).children("ul").children("li").removeClass("active");
+                $(tabs).children("ul").children("li").eq(i).addClass("active");
+            }
+
+            showPage(0);
+
+            $(tabs).children("ul").children("li").each(function(index, element){
+                $(element).attr("data-page", i);
+                i++;
+            });
+
+            $(tabs).children("ul").children("li").click(function(){
+                showPage(parseInt($(this).attr("data-page")));
+            });
+        };
+        return this.each(createTabs);
+    };
+})(jQuery);
+$(document).ready(function(){
+    $(".tabs").lightTabs();
 });
